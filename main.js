931c32fe33c89541971f2ea8b095a3f13dc33da2
NAM KHANG <namkhang0807@gmail.com>
2025-11-02 20:32:36 +0700

    cherry

--- main.js
+++ main.js
@@ -6,4 +6,5 @@ order.addProduct(new Product("Book", 10));
 order.addProduct(new Product("Pen", 5));
 
 console.log("Total:", order.getTotal());
-console.log("Say hello");
+console.log("Cherry:", order.getTotal());
+
